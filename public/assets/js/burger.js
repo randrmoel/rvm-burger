e3faf7ed1a934ce70a745855ea9e893ef069d999
Robert Moel <58125997+randrmoel@users.noreply.github.com>
2020-03-31 12:32:02 -0400

    Added comments

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,10 +1,13 @@
+// Encapsulating function for ajax calls from sheet
 $(function(){
+    //Add Burger button listener
     $("#addburger").on("click", function(event){
         event.preventDefault();
         console.log("submit clicked");
         var newBurger ={
             burger_name: $("#newburger").val().trim()
         };
+        //Must enter a burger, no blanks allowed
         if(newBurger.burger_name.length>0){
         $.ajax("/api/brgrs",{
             type: "POST",
@@ -17,7 +20,7 @@ $(function(){
         alert("Must enter burger name");
     }
     });
-
+    //call for devouring burger and changing to devoured side
     $(".eatit").on("click", function(){
         event.preventDefault();
         var id =$(this).data("id");
