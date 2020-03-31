07dae0eb97011b3ceaa9009709e907d0255c3f35
Robert Moel <58125997+randrmoel@users.noreply.github.com>
2020-03-31 12:25:41 -0400

    Add comments
    Added comments per section

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,21 @@
+// Import needed packages
 const mysql = require("mysql");
 var conn;
 
+//Set up Heroku database for deployment, if local use local host
+//Password removed
 if (process.env.JAWSDB_URL){
     conn = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     conn = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'Emerald1',
+        password: '',
         database: 'burgers_db'
     });
 };
 
+//Try connecting
 conn.connect((err)=>{
     if (err) {
         console.log('Error Connection: ' + err.stack);
@@ -19,4 +23,5 @@ conn.connect((err)=>{
 }
     console.log("Connected as ID: " + conn.threadId);
 });
+//export connection
 module.exports = conn;
